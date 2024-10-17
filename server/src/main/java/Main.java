8c19b15c2fd249822b41adf5f69a50d62171a894
amandaweller <amandazariweller@gmail.com>
2024-10-17 17:50:04 -0600

    new changes

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        Server server = new Server();
+        int port = 8080;
+        server.run(port);
     }
+    
 }
