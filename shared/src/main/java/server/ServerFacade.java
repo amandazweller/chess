8dd1c77ae278087ce253d15be5da8c417d532d4c
amandaweller <amandazariweller@gmail.com>
2024-11-04 21:27:50 -0700

    new changes

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,12 +1,17 @@
 package server;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.sun.net.httpserver.Request;
+import exception.ResponseException;
 import model.GameData;
 import model.AuthData;
+import model.ListGameResponse;
 import model.UserData;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 
 public class ServerFacade {
     private final String serverUrl;
@@ -15,8 +20,31 @@ public class ServerFacade {
         serverUrl = url;
     }
 
-    public UserData login(UserData userData) throws ResponseException {
+    public UserData registerUser(UserData userData) throws ResponseException {
+        return null;
+    }
+
+    private UserData loginUser(UserData userdata) throws ResponseException{
+        return null;
+    }
+
+    private void logoutUser(UserData userData) throws ResponseException {
 
     }
 
+    private ArrayList<GameData> listGames() throws ResponseException {
+        return null;
+    }
+
+    private GameData createGame(GameData gameData) throws ResponseException  {
+        return null;
+    }
+
+    private GameData joinGame(int gameID) throws ResponseException {
+        return null;
+    }
+
+    private void clearAll() throws ResponseException {
+
+    }
 }
