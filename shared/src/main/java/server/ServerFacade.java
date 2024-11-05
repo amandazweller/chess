8f7e18f1fe0822ccdf19d531a4ae0610d30c5c03
amandaweller <amandazariweller@gmail.com>
2024-11-04 21:34:11 -0700

    new changes

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -21,30 +21,91 @@ public class ServerFacade {
     }
 
     public UserData registerUser(UserData userData) throws ResponseException {
-        return null;
+        var path = "/user";
+        return this.makeRequest("POST", path, userData, UserData.class);
     }
 
-    private UserData loginUser(UserData userdata) throws ResponseException{
-        return null;
+    private UserData loginUser(UserData userData) throws ResponseException{
+        var path = "/session";
+        return this.makeRequest("POST", path, userData, UserData.class);
     }
 
     private void logoutUser(UserData userData) throws ResponseException {
-
+        var path = "/session";
+        this.makeRequest("DELETE", path, null, null);
     }
 
     private ArrayList<GameData> listGames() throws ResponseException {
-        return null;
+        var path = "/game";
+        record listPetResponse(ArrayList<GameData> games) {
+        }
+        var response = this.makeRequest("GET", path, null, listPetResponse.class);
+        return response.games();
     }
 
     private GameData createGame(GameData gameData) throws ResponseException  {
-        return null;
+        var path = "/game";
+        return this.makeRequest("POST", path, gameData, GameData.class);
     }
 
     private GameData joinGame(int gameID) throws ResponseException {
-        return null;
+        var path = "/game";
+        return this.makeRequest("POST", path, gameID, GameData.class);
     }
 
     private void clearAll() throws ResponseException {
-
+        var path = "/db";
+        this.makeRequest("DELETE", path, null, null);
     }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new ResponseException(status, "failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+
 }
