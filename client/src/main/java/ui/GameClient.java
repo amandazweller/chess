a498c993553ea504a2cf4be3e2a31d8467bc80ff
amandaweller <amandazariweller@gmail.com>
2024-11-30 17:29:52 -0700

    new changes

--- client/src/main/java/ui/GameClient.java
+++ client/src/main/java/ui/GameClient.java
@@ -1,4 +1,65 @@
 package ui;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import model.GameData;
+import exception.ResponseException;
+import client.ServerFacade;
+
+
 public class GameClient {
-}
+    ServerFacade serverFacade;
+
+
+    public GameClient(ServerFacade server, ReplGame replGame) {
+        serverFacade = server;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "redraw" -> redrawBoard();
+                case "move" -> makeMove(params);
+                case "highlightMoves" -> highlightMoves();
+                case "resign" -> resign();
+
+                case "leave" -> "leave";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    private String highlightMoves() {
+        return null;
+    }
+
+    private String makeMove(String[] params) {
+        return null;
+    }
+
+    private String redrawBoard() {
+        return null;
+    }
+
+    private String resign() {
+        return null;
+    }
+
+    public String help() {
+        return """
+                    create <NAME> - a game
+                    list - games
+                    join <ID> [WHITE|BLACK] - a game
+                    observe <ID> - a game
+                    logout - when you are done
+                    quit - playing chess
+                    help - with possible commands
+                    """;
+    }
+    }
--- client/src/main/java/ui/ReplGame.java
+++ client/src/main/java/ui/ReplGame.java
@@ -1,4 +1,49 @@
 package ui;
 
+import client.ServerFacade;
+
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+
+
+import static java.awt.Color.*;
+
+
 public class ReplGame {
+    public GameClient client;
+
+    public ReplGame(ServerFacade server){
+        client = new GameClient(server, this);
+
+    }
+
+    public void run(ServerFacade server) {
+        client = new GameClient(server, this);
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+            try {
+                result = client.eval(line);
+                System.out.print(result);
+                if (result.contains("left game")){
+                    break;
+                }
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+            System.out.println();
+        }
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET_TEXT_COLOR + RESET_BG_COLOR);
+    }
+
+
 }
