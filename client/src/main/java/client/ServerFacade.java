78e1112cffdfe0bce43dc228e332ec5732df7a5b
amandaweller <amandazariweller@gmail.com>
2024-12-02 17:41:45 -0700

    new changes

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,13 +1,16 @@
 package client;
 
 import chess.ChessGame;
+import chess.ChessMove;
 import chess.ChessPosition;
 import client.websocket.HttpFacade;
+import client.websocket.WebSocketFacade;
 import com.google.gson.Gson;
 import exception.ResponseException;
 import model.GameData;
 import model.ListGameResponse;
 import ui.PrintBoard;
+import websocket.commands.*;
 
 import java.io.*;
 import java.net.*;
@@ -22,53 +25,177 @@ public class ServerFacade {
     public ChessGame game;
     public String currentUsername;
     public boolean playing = false;
-    HttpFacade httpFacade;
+
+    WebSocketFacade webSocketFacade;
 
     public ServerFacade(String url){
         serverUrl = url;
     }
 
-    public ServerFacade(){
-        this("http://localhost:8181");
-        httpFacade = new HttpFacade(this, serverUrl);
-    }
-
-
-
     public boolean registerUser(String username, String password, String email) throws ResponseException {
-        return httpFacade.registerUser(username, password, email);
+        var body = Map.of("username", username, "password", password, "email", email);
+        var jsonBody = new Gson().toJson(body);
+        var path = "/user";
+        var response = this.makeRequest("POST", path, jsonBody);
+        if (response.contains("Error")){
+            return false;
+        }
+        Map responseMap = new Gson().fromJson(response, Map.class);
+        authToken = (String) responseMap.get("authToken");
+        return !response.contains("Error");
     }
 
     public boolean loginUser(String username, String password) throws ResponseException{
-        return httpFacade.loginUser(username, password);
+        var body = Map.of("username", username, "password", password);
+        var jsonBody = new Gson().toJson(body);
+        var path = "/session";
+        var response = this.makeRequest("POST", path, jsonBody);
+        if (response.contains("Error")){
+            return false;
+        }
+        Map responseMap = new Gson().fromJson(response, Map.class);
+        authToken = (String) responseMap.get("authToken");
+        currentUsername = username;
+        return !response.contains("Error");
     }
 
     public boolean logoutUser() throws ResponseException {
-        return httpFacade.logoutUser();
-    }
+        var path = "/session";
+        var response = this.makeRequest("DELETE", path, null);
+        if (response.contains("Error")){
+            return false;
+        }
+        authToken = null;
+        currentUsername = null;
+        return !response.contains("Error");    }
 
     public ArrayList<GameData> listGames() throws ResponseException {
-        return httpFacade.listGames();
+        var path = "/game";
+        var response = this.makeRequest("GET", path, null);
+        if (response.contains("Error")) {
+            return new ArrayList<>();
+        }
+        ListGameResponse games = new Gson().fromJson(response, ListGameResponse.class);
+        return games.games();
     }
 
     public boolean createGame(String gameName) throws ResponseException  {
-        return httpFacade.createGame(gameName);
+        var body = Map.of("gameName", gameName);
+        var jsonBody = new Gson().toJson(body);
+        var path = "/game";
+        var response = makeRequest("POST", path, jsonBody);
+        return !response.contains("Error");
     }
 
     public boolean joinGame(GameData gameData, String playerColor) throws ResponseException {
-        return httpFacade.joinGame(gameData, playerColor);
+        var body = Map.of("gameID", gameData.gameID(), "playerColor", playerColor);
+        teamColor = ChessGame.TeamColor.valueOf(playerColor);
+        currentGameID = gameData.gameID();
+        game = gameData.game();
+        playing = true;
+        var jsonBody = new Gson().toJson(body);
+        var path = "/game";
+        var response = this.makeRequest("PUT", path, jsonBody);
+        return !response.contains("Error");
     }
 
     public void clearAll() throws ResponseException {
-        httpFacade.clearAll();
+        var path = "/db";
+        this.makeRequest("DELETE", path, null);
+    }
+
+    private String makeRequest(String method, String path, String request) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            if (authToken != null) {
+                http.addRequestProperty("authorization", authToken);
+            }
+
+            if (request != null) {
+                http.setDoOutput(true);
+                http.setRequestProperty("Content-Type", "application/json");
+                try (OutputStream os = http.getOutputStream()) {
+                    os.write(request.getBytes());
+                    os.flush();
+                }
+            }
+
+            http.connect();
+            int responseCode = http.getResponseCode();
+            if (!isSuccessful(responseCode)) {
+                return "Error: 401";
+            }
+            return readBody(http);
+
+        } catch (IOException e) {
+            throw new ResponseException(500, "Connection error: " + e.getMessage());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String readBody(HttpURLConnection http) throws IOException {
+        try (InputStream inputStream = http.getInputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            return response.toString();
+        }
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
     }
 
     public boolean observeGame(int gameID) {
-        return httpFacade.observeGame(gameID);
+        currentGameID = gameID;
+        teamColor = null;
+        return true;
     }
 
     public void printBoard(ChessPosition highlighted) throws ResponseException {
         new PrintBoard(game).printBoard(teamColor, highlighted);
     }
 
+    public void connect() {
+        try {
+            webSocketFacade = new WebSocketFacade(serverUrl, teamColor);
+        }
+        catch (Exception e) {
+            System.out.println("Failed to make connection with server");
+        }
+    }
+
+    public void sendCommand(UserGameCommand command) {
+        String message = new Gson().toJson(command);
+        webSocketFacade.sendMessage(message);
+    }
+
+    public void joinObserver(int gameID) {
+        sendCommand(new JoinObserver(authToken, gameID));
+    }
+
+    public void joinPlayer(int gameID, ChessGame.TeamColor color) {
+        sendCommand(new JoinPlayer(authToken, gameID, color));
+    }
+
+    public void makeMove(int gameID, ChessMove move) {
+        sendCommand(new MakeMove(authToken, gameID, move));
+    }
+
+    public void leave(int gameID) {
+        sendCommand(new Leave(authToken, gameID));
+    }
+
+    public void resign(int gameID) {
+        sendCommand(new Resign(authToken, gameID));
+    }
 }
